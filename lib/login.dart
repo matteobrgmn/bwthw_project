9086c7372d976915a0ebefa4771bd97c764679bd
GuglielmiCh <christian.guglielmi@studenti.unipd.it>
2026-05-05 12:17:00 +0200

    Add files via upload

---
+++ lib/login.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+//  runApp(const MyApp()); SEGMENTO UTILE PER TESTARE LA PAGINA DA SOLA
+//}
+
+List<bool> _selectedOption = [true, false];
+
+const List<Widget> options = <Widget>[Text("Login"),Text("Sign up")];
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const LoginPage(title: 'Start your journey'),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key, required this.title});
+  final String title;
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .start,
+          children: [
+            SizedBox(height:50,),
+            const Text("Please input login details"),
+            SizedBox(height: 5,),
+            ToggleButtons( 
+              direction: Axis.horizontal,
+               onPressed: (int index) {
+                  setState(() {
+                    // The button that is tapped is set to true, and the others to false.
+                    for (int i = 0; i < _selectedOption.length; i++) {
+                      _selectedOption[i] = i == index;
+                    }
+                  });
+                },
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                selectedBorderColor: Colors.deepPurpleAccent[700],
+                selectedColor: Colors.white,
+                fillColor: const Color.fromARGB(255, 169, 100, 181),
+                color: Colors.deepPurpleAccent[400],
+                constraints: const BoxConstraints(
+                  minHeight: 40.0,
+                  minWidth: 80.0,
+                ),
+                isSelected: _selectedOption,
+                children: options,
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20,0,20,0),
+                child: TextField(
+                  decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'Enter your username',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(),
+                ),
+               ),
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20,0,20,0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  prefixIcon: Icon(Icons.key),
+                  border: OutlineInputBorder(),  
+                ),
+               ),
+              ),
+              SizedBox(height: 10,),
+              ElevatedButton(onPressed: (){
+                Navigator.of(context).pop();
+              }, child: _selectedOption[0]?Text("Log in"):Text("Sign in")),
+           ],
+          ),
+        ),
+      );
+  }
+}
