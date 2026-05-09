5e9c4f70b2dfbd73ab10b0d8345cc0be4ba72752
Matteo <bergaminmatteo1998@gmail.com>
2026-05-09 19:09:21 +0200

    Improvements on file management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'impact/impact.dart';
-import 'package:fl_chart/fl_chart.dart';
-
+import 'screens/debugPage.dart';
 void main() {
   runApp(MainApp());
 }
@@ -14,95 +12,11 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
 
-  final impact = Impact();
-  List<FlSpot> spots = [];
-
   String serverStatus = 'offline';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center (
-          child : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children : [
-            // Button to verify the server status
-            ElevatedButton(onPressed: () async {
-              serverStatus = await impact.serverStatus();
-              setState(() {
-              });
-            }, child: Text('Test ping')),
-            Text('Server $serverStatus'),
-            ElevatedButton(onPressed: () async {
-              await impact.authentication();
-            }, child: Text('Authentication')),
-            ElevatedButton(onPressed: () async {
-              impact.printer();
-            }, child: Text('Printer')),
-            ElevatedButton(onPressed: () async {
-              await impact.getStepsSingleDay('2024-05-04');
-            }, child: Text('Get steps')),
-            ElevatedButton(onPressed: () async {
-              await impact.refresh();
-            }, child: Text('Refresh')),
-            ElevatedButton(onPressed: () async {
-              final mapOfDates = await impact.getStepsBtwTwoDates('2024-05-04', '2024-05-11');
-              // print(mapOfDates); // Debug
-              var i = 0;
-              spots = [];
-              mapOfDates.forEach((key, value) {
-                spots.add(
-                    FlSpot(i.toDouble(), value.toDouble()),
-                  );
-                  i += 1;
-              });
-              // print(spots); // Debug
-              setState(() {
-                
-              });
-            }, child: Text('Get steps between two dates')), 
-
-            // Small chart to represent steps data
-            SizedBox(
-            height: 300,
-            width : 300,
-            child: LineChart(
-              LineChartData(
-                minX: 0,
-                maxX: 7,
-                minY: 0,
-                maxY: 40000,
-                gridData: FlGridData(show:false),
-                // To remove description from right and the top
-                titlesData: FlTitlesData(
-                  topTitles: AxisTitles(
-                    sideTitles: SideTitles(showTitles: false),
-                  ),
-                  rightTitles: AxisTitles(
-                    sideTitles: SideTitles(showTitles: false),
-                  ),
-                  leftTitles: AxisTitles(
-                    sideTitles: SideTitles(showTitles: true),
-                  ),
-                  bottomTitles: AxisTitles(
-                    sideTitles: SideTitles(showTitles: true),
-                  ),
-                ),
-                lineBarsData: [
-                  LineChartBarData(
-                    spots : spots,
-                    isCurved: false,
-                    barWidth: 2,
-                  )
-                ]
-              )
-            )
-          )
-        ]
-        ),
-        ),
-      ),
-      );
+      home : DebugPage(),
+    );
   }
 }
---
+++ lib/screens/debugPage.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import '../impact/impact.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class DebugPage extends StatefulWidget {
+  
+
+  @override
+  State<DebugPage> createState() => _DebugPageState();
+}
+
+class _DebugPageState extends State<DebugPage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+
+  final impact = Impact();
+  List<FlSpot> spots = [];
+  String serverStatus = 'offline';
+
+    return Scaffold(
+        body: Center (
+          child : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children : [
+            // Button to verify the server status
+            ElevatedButton(onPressed: () async {
+              serverStatus = await impact.serverStatus();
+              setState(() {
+              });
+            }, child: Text('Test ping')),
+            Text('Server $serverStatus'),
+            ElevatedButton(onPressed: () async {
+              await impact.authentication();
+            }, child: Text('Authentication')),
+            ElevatedButton(onPressed: () async {
+              impact.printer();
+            }, child: Text('Printer')),
+            ElevatedButton(onPressed: () async {
+              await impact.getStepsSingleDay('2024-05-04');
+            }, child: Text('Get steps')),
+            ElevatedButton(onPressed: () async {
+              await impact.refresh();
+            }, child: Text('Refresh')),
+            ElevatedButton(onPressed: () async {
+              final mapOfDates = await impact.getStepsBtwTwoDates('2024-05-04', '2024-05-11');
+              // print(mapOfDates); // Debug
+              var i = 0;
+              spots = [];
+              mapOfDates.forEach((key, value) {
+                spots.add(
+                    FlSpot(i.toDouble(), value.toDouble()),
+                  );
+                  i += 1;
+              });
+              // print(spots); // Debug
+              setState(() {
+                
+              });
+            }, child: Text('Get steps between two dates')), 
+
+            // Small chart to represent steps data
+            SizedBox(
+            height: 300,
+            width : 300,
+            child: LineChart(
+              LineChartData(
+                minX: 0,
+                maxX: 7,
+                minY: 0,
+                maxY: 40000,
+                gridData: FlGridData(show:false),
+                // To remove description from right and the top
+                titlesData: FlTitlesData(
+                  topTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  rightTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: true),
+                  ),
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: true),
+                  ),
+                ),
+                lineBarsData: [
+                  LineChartBarData(
+                    spots : spots,
+                    isCurved: false,
+                    barWidth: 2,
+                  )
+                ]
+              )
+            )
+          )
+        ]
+        ),
+      ),
+    );
+  }
+}
