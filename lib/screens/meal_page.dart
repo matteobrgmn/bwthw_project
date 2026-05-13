d0f8434a38a71dbaec898b86a3175c1a66f1892a
MatteoGuglielmi-Tech <matteo.guglielmi.ma.gu@gmail.com>
2026-05-13 12:52:45 +0200

    feat(meal_page): add meal list management buttons

--- lib/screens/meal_page.dart
+++ lib/screens/meal_page.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 
-//void main() {
-//  runApp(const MyApp()); SEGMENTO UTILE PER TESTARE LA PAGINA DA SOLA
-//}
+void main() {
+  // DEBUGGING SEGMENT
+  runApp(MyApp());
+}
 
-List<bool> _selectedOption = [true, false];
-
-const List<Widget> options = <Widget>[Text("New Meal"),Text("Meal List")];
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      title: 'Meal Page',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MealPage(title: 'Meal register'),
     );
   }
@@ -30,28 +26,106 @@ class MealPage extends StatefulWidget {
 }
 
 class _MealPageState extends State<MealPage> {
+  final List<TextEditingController> _controllers = [];
+  int numMeal = 1;
+
+  @override
+  void dispose() {
+    for (final c in _controllers) {
+      c.dispose();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: .start,
           children: [
-            SizedBox(height:50,),
-              ElevatedButton(onPressed: () async{
-                // TODO: implementare menu modulare di aggiunta pasti
-                // TO assess: LLM-powered??
-              }, child: Icon(Icons.add)),
-              ElevatedButton(onPressed: () async {
+            SizedBox(height: 50),
+            // TODO: implementare menu modulare di aggiunta pasti
+            // TO assess: LLM-powered??
+            Title(color: Colors.black, child: Text("Meal update")),
+
+            //makes a list of all the entries of the menu
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: _controllers.asMap().entries.map((e) {
+                  return Row(
+                    key: ValueKey(e.key),
+                    mainAxisAlignment: .center,
+                    children: [
+                      Text("Menu entry #${e.key + 1}"),
+                      Expanded(
+                        child: TextField(
+                          controller: e.value,
+                          decoration: InputDecoration(
+                            hintText: 'Enter the ingredient',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _controllers.add(TextEditingController());
+                  numMeal++;
+                });
+              },
+
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(20),
+                backgroundColor: const Color.fromARGB(255, 171, 97, 184),
+              ),
+              child: Icon(
+                Icons.add,
+                color: Colors.deepPurpleAccent[400],
+                size: 30,
+              ),
+            ),
+
+            //generazione condizionata di un elevatedButton che rimuove gli elementi dei pasti se esiste almeno un alimento nel menu
+            numMeal > 1
+                ? ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _controllers.last.dispose();
+                        _controllers.removeLast();
+                        numMeal--;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(20),
+                      backgroundColor: const Color.fromARGB(255, 169, 100, 181),
+                    ),
+                    child: Icon(
+                      Icons.remove_circle,
+                      color: Colors.deepPurpleAccent[400],
+                      size: 30,
+                    ),
+                  )
+                : SizedBox(height: 0),
+            /* ElevatedButton(onPressed: () async {
                 Navigator.pop(context);
-              }, child: Text("Homepage")),             
-           ],
-          ),
+              }, child: Text("Homepage")),     */
+          ],
         ),
-      );
+      ),
+    );
   }
 }
